c8be753bbe4c2585d1d004d45442aaa02c9c2aa5
Anand Chowdhary <anandchowdhary@gmail.com>
2018-01-26 17:31:14 +0100

    Print final array of binary values to serial

--- ardunio/drumpad/drumpad.ino
+++ ardunio/drumpad/drumpad.ino
@@ -15,6 +15,7 @@ CapacitiveSensor capacitiveSensors[4] = {
 	CapacitiveSensor(8, 9),
 	CapacitiveSensor(6, 7)
 };
+int previousValues[4] = { 0, 0, 0, 0 };
 
 void setup() {
 
@@ -24,7 +25,10 @@ void setup() {
 	// capacitiveSensor3.set_CS_AutocaL_Millis(0xFFFFFFFF);
 	// capacitiveSensor4.set_CS_AutocaL_Millis(0xFFFFFFFF);
 
-	pinMode(2, OUTPUT);
+	// Setting pins 2 to 5 to output for LEDs
+	for (int i = 2; i < 6; i++) {
+		pinMode(i, OUTPUT);
+	}
 
 	// Prepare to send data on 9600
 	Serial.begin(9600);
@@ -33,12 +37,29 @@ void setup() {
 
 void loop() {
 
+	String sendValues = "[ ";
+
+	for (int i = 0; i < 4; i++) {
+		previousValues[i] = 0;
+		digitalWrite(i + 2, LOW);
+		if (capacitiveSensors[i].capacitiveSensor(30) > 500 && previousValues[i] == 0) {
+			previousValues[i] = 1;
+			digitalWrite(i + 2, HIGH);
+		}
+		sendValues += String(previousValues[i]) + " ";
+	}
+
+	sendValues += "]";
+
+	if (sendValues != "[ 0 0 0 0 ]") {
+		Serial.println(sendValues);
+	}
+
 	// long start = millis();
 	// long total1 = capacitiveSensor1.capacitiveSensor(30);
 	// Serial.print(millis() - start); // check on performance in milliseconds
-	Serial.println(String(capacitiveSensors[0].capacitiveSensor(30)) + " " + String(capacitiveSensors[1].capacitiveSensor(30)) + " " + String(capacitiveSensors[2].capacitiveSensor(30)) + " " + String(capacitiveSensors[3].capacitiveSensor(30)));
+	// Serial.println(String(capacitiveSensors[0].capacitiveSensor(30)) + " " + String(capacitiveSensors[1].capacitiveSensor(30)) + " " + String(capacitiveSensors[2].capacitiveSensor(30)) + " " + String(capacitiveSensors[3].capacitiveSensor(30)));
 	// print sensor output 1 - this is also the value you can use to use this in other projects
-	digitalWrite(2, LOW);
-	delay(500); // arbitrary delay to limit data to serial port
+	delay(50); // arbitrary delay to limit data to serial port
 
 }
