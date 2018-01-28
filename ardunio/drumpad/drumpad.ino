ffed69b33ecc91835187afabeaeca067caa5645e
Anand Chowdhary <mail@anandchowdhary.com>
2018-01-28 16:50:01 +0100

    Add comments on Arduino

--- ardunio/drumpad/drumpad.ino
+++ ardunio/drumpad/drumpad.ino
@@ -20,10 +20,6 @@ String prevSendValue = "0000";
 
 void setup() {
 
-	// Turning off recalibration for all sensors
-	// I've hardcoded the sensor input limit for now
-	// capacitiveSensor4.set_CS_AutocaL_Millis(0xFFFFFFFF);
-
 	// Setting pins 2 to 5 to output for LEDs
 	for (int i = 2; i < 6; i++) {
 		pinMode(i, OUTPUT);
@@ -36,31 +32,40 @@ void setup() {
 
 void loop() {
 
+	// Initialize `sendValues` variable
 	String sendValues = "";
 
+	// Send potentiometer and volume readings
 	Serial.println("Potentiometer reading: " +  String(analogRead(A1)));
 	Serial.println("Volume reading: " +  String(analogRead(A0)));
 
+	// Loop through each instrument value
 	for (int i = 0; i < 4; i++) {
+
+		// By default, user has not tapped
+		// and corresponsing LED is off
 		previousValues[i] = 0;
 		digitalWrite(i + 2, LOW);
+
+		// Check if user has tapped on instrument
 		if (capacitiveSensors[i].capacitiveSensor(30) > 500 && previousValues[i] == 0) {
 			previousValues[i] = 1;
+			// Turn LED on in case they have
 			digitalWrite(i + 2, HIGH);
 		}
+	
+		// Append to `sendValues` variable
 		sendValues += String(previousValues[i]);
+
 	}
 
+	// Send to serial if it's different from the previously
+	// sent variable value
 	if (sendValues != prevSendValue) {
 		Serial.println(sendValues);
 		prevSendValue = sendValues;
 	}
 
-	// long start = millis();
-	// long total1 = capacitiveSensor1.capacitiveSensor(30);
-	// Serial.print(millis() - start); // check on performance in milliseconds
-	// Serial.println(String(capacitiveSensors[0].capacitiveSensor(30)) + " " + String(capacitiveSensors[1].capacitiveSensor(30)) + " " + String(capacitiveSensors[2].capacitiveSensor(30)) + " " + String(capacitiveSensors[3].capacitiveSensor(30)));
-	// print sensor output 1 - this is also the value you can use to use this in other projects
-	delay(50); // arbitrary delay to limit data to serial port
+	delay(50); // Arbitrary delay to limit data to serial port
 
 }
